4e1a1bcca27c7a1cc84a5cdc734b36de9e7f505e
Alex Psitos <apsitos08@gmail.com>
2022-07-08 18:18:29 -0400

    Test: dog model test written and passed

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,5 +3,6 @@ class Dog < ApplicationRecord
                           :age,
                           :breed 
     belongs_to :user 
+    has_many :dog_skills
     has_many :skills, through: :dog_skills 
 end 
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+    factory :user do 
+        Faker::Config.locale = 'en-US'
+        username { Faker::Internet.unique.username}
+        email { Faker::Internet.email}
+    end 
+end 
