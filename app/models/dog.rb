f3b0ded54228a607810c304165c18cf26d37a414
Alex Psitos <apsitos08@gmail.com>
2022-07-08 17:36:36 -0400

    Create dog model

---
+++ app/models/dog.rb
@@ -0,0 +1,7 @@
+class Dog < ApplicationRecord
+    validates_presence_of :name,
+                          :age,
+                          :breed 
+    belongs_to :user 
+    has_many :skills, through: :dog_skills 
+end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     validates_presence_of :username,
                           :email
     validates_uniqueness_of :email 
+
+    has_many :dogs 
 end 
