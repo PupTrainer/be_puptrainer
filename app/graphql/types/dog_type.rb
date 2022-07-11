6db2e695b74fb348505f6a36285ba5b5c0302b7c
Alicia Watt <alicia.watt.aw@gmail.com>
2022-07-11 14:25:32 -0600

    remove pry

--- app/graphql/types/dog_type.rb
+++ app/graphql/types/dog_type.rb
@@ -10,5 +10,10 @@ module Types
     field :user_id, Integer
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :user, Types::UserType, null: true
+    def user
+      user = User.find(self.object.user_id)
+      user
+    end 
   end
 end
