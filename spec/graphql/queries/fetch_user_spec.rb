75f1bd7b33e7eab28785b0a53d3e3b2f13fb1971
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-07-12 13:49:21 -0600

    Test: add fetch_dog_spec, add initial expectation for fetchUser endpoint

---
+++ spec/graphql/queries/fetch_user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+module Queries
+  RSpec.describe FetchUser, type: :request do 
+    describe '.resolve' do 
+      it 'returns a user with any dogs they have' do 
+        user1 = User.create!(username: 'lron_hubbard', email: 'lron@spaghetti.com')
+        dog1 = Dog.create!(name: "Tim Cruise", age: 6, breed: 'Dachshund', user_id: user1.id)
+        dog2 = Dog.create!(name: "50 Cent", age: 2, breed: 'German Shorthair Pointer', user_id: user1.id)
+        
+        post '/graphql', params: { query: query }
+        json = JSON.parse(response.body, symbolize_names: true)
+        data = json[:data][:fetchUser]
+        expect(data).to be_a Hash
+      end
+    end
+
+  end
+end
