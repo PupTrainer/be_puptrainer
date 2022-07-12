de60924129735e31f7af0017c0acf29728405947
Alex Psitos <apsitos08@gmail.com>
2022-07-12 15:50:22 -0400

    Feat: update user model to include unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates_presence_of :username,
     :email
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :username 
 
   has_many :dogs
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    it { should validate_uniqueness_of(:username) }
   end
 
   describe "relationships" do
