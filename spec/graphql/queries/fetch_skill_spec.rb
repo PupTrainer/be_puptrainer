0fe2557b76cc5808cea644839f826355069ea13b
Alex Psitos <apsitos08@gmail.com>
2022-07-12 19:20:14 -0400

    Test: write test for find skill by id happy and sad path

--- spec/graphql/queries/fetch_skill_spec.rb
+++ spec/graphql/queries/fetch_skill_spec.rb
@@ -8,8 +8,41 @@ module Queries
                 
                 post '/graphql', params: { query: query }
                 json = JSON.parse(response.body, symbolize_names: true)
+                data = json[:data][:fetchSkill]
+
+                expect(data).to be_a Hash
+
+                expect(data).to have_key(:name)
+                expect(data[:name]).to be_a String
+
+                expect(data).to have_key(:level)
+                expect(data[:level]).to be_a Integer
+
+                expect(data).to have_key(:description)
+                expect(data[:description]).to be_a String
+
+                expect(data).to have_key(:criteria)
+                expect(data[:criteria]).to be_a String
+
+                expect(data).to have_key(:youtubeLink)
+                expect(data[:youtubeLink]).to be_a String
+                
             end 
+
         end 
+
+        describe 'sad path' do 
+            it 'returns an error message if given an invalid id' do 
+            
+                post "/graphql", params: {query: query_2}
+                json = JSON.parse(response.body, symbolize_names: true)
+                
+                data = json[:errors]
+
+                expect(data[0]).to eq({:message=>"Skill does not exist.", :locations=>[{:line=>2, :column=>5}], :path=>["fetchSkill"]})
+                
+            end
+        end  
         
         def query
             <<~GQL
@@ -19,7 +52,21 @@ module Queries
                         level
                         description
                         criteria
-                        youtube_link
+                        youtubeLink
+                    }
+                }
+            GQL
+        end 
+
+        def query_2
+            <<~GQL
+                query {
+                    fetchSkill(id: 2) {
+                        name
+                        level
+                        description
+                        criteria
+                        youtubeLink
                     }
                 }
             GQL
