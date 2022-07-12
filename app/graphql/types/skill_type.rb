ee522cf91ab80d9bfa708cfe299b83cbcce928af
Alex Psitos <apsitos08@gmail.com>
2022-07-12 19:21:50 -0400

    Fix: format skill type so that it is not looking for an array but a single skill

--- app/graphql/types/skill_type.rb
+++ app/graphql/types/skill_type.rb
@@ -3,11 +3,11 @@
 module Types
   class SkillType < Types::BaseObject
     field :id, ID, null: false
-    field :name, String
-    field :level, Integer
-    field :description, String
-    field :criteria, String
-    field :youtube_link, String
+    field :name, String 
+    field :level, Integer 
+    field :description, String 
+    field :criteria, String 
+    field :youtube_link, String 
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
