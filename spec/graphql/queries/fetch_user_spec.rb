dc39dd10349d62258d569478ebd4560bc927c0dd
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-07-12 13:56:37 -0600

    Test: add expectations for fetchuser returning a user and their attributes from a given id

--- spec/graphql/queries/fetch_user_spec.rb
+++ spec/graphql/queries/fetch_user_spec.rb
@@ -3,22 +3,56 @@ require 'rails_helper'
 module Queries
   RSpec.describe FetchUser, type: :request do 
     describe '.resolve' do 
-      it 'returns a user with any dogs they have' do 
-        user1 = User.create!(username: 'lron_hubbard', email: 'lron@spaghetti.com')
-        dog1 = Dog.create!(name: "Tim Cruise", age: 6, breed: 'Dachshund', user_id: user1.id)
-        dog2 = Dog.create!(name: "50 Cent", age: 2, breed: 'German Shorthair Pointer', user_id: user1.id)
+      before do
+        @user1 = User.create!(username: 'lron_hubbard', email: 'lron@spaghetti.com')
+        @dog1 = Dog.create!(name: "Tim Cruise", age: 6, breed: 'Dachshund', user_id: @user1.id)
+        @dog2 = Dog.create!(name: "50 Cent", age: 2, breed: 'German Shorthair Pointer', user_id: @user1.id)        
+      end
+      it 'returns a user from a given ID' do 
         
         post '/graphql', params: { query: query }
         json = JSON.parse(response.body, symbolize_names: true)
         data = json[:data][:fetchUser]
         expect(data).to be_a Hash
+        expect(data).to have_key(:id)
+        expect(data[:id]).to be_a String
+        
+        expect(data).to have_key(:username)
+        expect(data[:username]).to be_a String
+        expect(data[:username]).to eq(@user1.username)
+        
+        expect(data).to have_key(:email)
+        expect(data[:email]).to be_a String
+        expect(data[:email]).to eq(@user1.email)
+        
+        expect(data).to have_key(:dogs)
+        expect(data[:dogs]).to be_an Array
+      end
+
+      it 'returns a user and any dogs they have from a given ID' do 
+        
+        post '/graphql', params: { query: query }
+        json = JSON.parse(response.body, symbolize_names: true)
+        dogs = json[:data][:fetchUser]
+        expect(data).to be_a Hash
+        expect(data).to have_key(:id)
+        expect(data[:id]).to be_a String
+        
+        expect(data).to have_key(:username)
+        expect(data[:username]).to be_a String
+        expect(data[:username]).to eq(@user1.username)
+        
+        expect(data).to have_key(:email)
+        expect(data[:email]).to be_a String
+        expect(data[:email]).to eq(@user1.email)
+
       end
     end
 
     def query
       <<~GQL
         query {
-          fetchUser(id: 1){
+          fetchUser(id: #{@user1.id}){
             id
             username
             email
