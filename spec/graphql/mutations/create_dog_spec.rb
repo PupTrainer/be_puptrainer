a410c0925cebd6e4ca2df6df0992816185c94ef8
Alicia Watt <alicia.watt.aw@gmail.com>
2022-07-13 15:23:58 -0600

    add dog count to test

--- spec/graphql/mutations/create_dog_spec.rb
+++ spec/graphql/mutations/create_dog_spec.rb
@@ -1,13 +1,15 @@
-require "rails_helper"
+require 'rails_helper'
 
 module Mutations
   RSpec.describe CreateDog, type: :request do
-    describe ".resolve" do
-      it "creates a dog" do
-        user = create(:user)
+    describe '.resolve' do
+      it 'creates a dog' do
+      
+        expect(Dog.count).to eq(0)
+        user = create_list(:user, 2)
         # dog = create(:dog, {user_id: user.id})
 
-        post "/graphql", params: {query: query}
+        post '/graphql', params: { query: query }
         json = JSON.parse(response.body, symbolize_names: true)
         data = json[:data][:createDog]
 
@@ -16,24 +18,32 @@ module Mutations
         expect(data[:age]).to be_a Integer
 
         expect(data[:breed]).to be_a String
+
+        expect(data[:user]).to be_a Hash
+        expect(Dog.count).to eq(1)
       end
       def query
         <<~GQL
-          mutation {
-            createDog(input: {
+                mutation {
+                  createDog(input: {
 
-              name: "Cujo"
-              age: 2
-              breed: "Saint Bernard"
-              userId: 1
-            })  {
-                id
-                name
-                age
-                breed
-              }
-
+                    name: "Cujo"
+                    age: 2
+                    breed: "Saint Bernard"
+                    userId: 2
+                  })  {
+                      id
+                      name
+                      age
+                      breed
+                      user {
+            id
+            username
+            email
           }
+                    }
+
+                }
         GQL
       end
     end
