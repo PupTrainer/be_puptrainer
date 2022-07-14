3a7621799cfd60327ac309553b656d8ebf343c39
Joseph Senecal <jasenecal2@gmail.com>
2022-07-13 19:47:24 -0600

    Test: build integration test for pass_dog_skill mutation

--- spec/graphql/mutations/update_dog_skill_spec.rb
+++ spec/graphql/mutations/update_dog_skill_spec.rb
@@ -3,12 +3,13 @@ require "rails_helper"
 module Mutations
   RSpec.describe PassDogSkill, type: :request do
     describe ".resolve" do
-      it "update a dog's skill 'passed' status to true" do
+      it "update a dog skill's 'passed' status to true" do
         user = create(:user)
         dog = create(:dog, user_id: user.id)
-        dog_skill = dog.dog_skills.create!(passed: false, dog_id: dog.id, skill_id: 1, created_at: Time.parse("2022.07.12"), updated_at: Time.parse("2022.07.12"))
+        skill = Skill.create!(name: "Oh You", level: 3 , description: "Running and Barking will no longer annoy your owners", criteria: " When running and barking, humans should not correct the behavior, but should sign and say 'oh, you....'" , youtube_link: "AtuxAJjygO8", created_at: Time.parse("2020.07.12"), updated_at: Time.parse("2020.07.12"))
+        dog_skill = dog.dog_skills.create!(passed: false, dog_id: dog.id, skill_id: skill.id, created_at: Time.parse("2022.07.12"), updated_at: Time.parse("2022.07.12"))
         
-        post "graphql/", params: {query: query}
+        post "graphql/", params: {query: query, variables: {dog_skill_id: dog_skill.id.to_s}}
         response_body = JSON.parse(response.body, symbolize_names: true)
 
         # Test response shape 
@@ -23,16 +24,16 @@ module Mutations
 
         # Test that update was timestamped 
         expect(response_body[:dog_skill][:updated_at]).to_not eq dog_skill.created_at
-        
+
         # Test that status was updated specifically to true
         expect(response_body[:dog_skill][:status]).to eq true
+      end
 
-      def query(dog_skill_id)
+      def query
         <<~GQL
           mutation {
             passSkill(input: {
-              dog_skill_id: dog_skill_id
-
+              $dog_skill_id: dog_skill_id!
             }) {
               dog_skill {
                 id
