40b6614f486473db7fbfd6621fe73f534cb1570e
Joseph Senecal <jasenecal2@gmail.com>
2022-07-14 14:58:30 -0600

    Fix: debug pass_dog_skill mutation

--- app/graphql/mutations/pass_dog_skill.rb
+++ app/graphql/mutations/pass_dog_skill.rb
@@ -1,17 +1,20 @@
 class Mutations::PassDogSkill < Mutations::BaseMutation
-    
     argument :dog_skill_id, Integer, required: true
 
-    field :dog_skill, Types::DogSkillType, null: false
+    field :dog_skill, Types::DogSkillType, null: true
     field :errors, [String], null: false
 
     def resolve(dog_skill_id:)
+        begin
         dog_skill =  DogSkill.find(dog_skill_id)
-        dog_skill.update(passed: true)
-        if dog_skill.passed
+        rescue ActiveRecord::RecordNotFound => e
+            raise GraphQL::ExecutionError, "Bad ID: dog_skill not found!"
+        end
+        dog_skill.update!(passed: true)
+        if dog_skill.passed == true
             {dog_skill: dog_skill, errors: []}
         else
-            {dog_skill: null, errors: ["Status failed to update"]}
+            raise GraphQL::ExecutionError, "Server Error: Your record was found, but the status failed to update."
         end
     end
 end
--- app/graphql/types/dog_skill_type.rb
+++ app/graphql/types/dog_skill_type.rb
@@ -3,9 +3,9 @@
 module Types
     class DogSkillType < Types::BaseObject
       field :id, ID, null: false
-      field :skill_id, Integer, null: false
-      field :dog_id, Integer, null: false
-      field :status, Boolean
+      field :skill_id, Integer
+      field :dog_id, Integer
+      field :passed, Boolean
       field :created_at, GraphQL::Types::ISO8601DateTime, null: false
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
