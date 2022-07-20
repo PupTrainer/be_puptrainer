d7e7b515ea2b74b4b9fe85db75bb61dc08286ed1
Joseph Senecal <jasenecal2@gmail.com>
2022-07-19 21:41:03 -0600

    Test: Refactor pass_dog_skill_mutation tests to expect dog_id and skill_id inputs

--- spec/graphql/mutations/pass_dog_skill_spec.rb
+++ spec/graphql/mutations/pass_dog_skill_spec.rb
@@ -10,10 +10,11 @@ module Mutations
         skill = Skill.create!(name: "Oh You", level: 3 , description: "Running and Barking will no longer annoy your owners", criteria: " When running and barking, humans should not correct the behavior, but should sign and say 'oh, you....'" , youtube_link: "AtuxAJjygO8", created_at: Time.parse("2020.07.12"), updated_at: Time.parse("2020.07.12"))
         dog_skill = dog.dog_skills.create!(passed: false, dog_id: dog.id, skill_id: skill.id, created_at: Time.parse("2022.07.12"), updated_at: Time.parse("2022.07.12"))
         
-        post "/graphql", params: {query: good_query(dog_skill.id)}
+        post "/graphql", params: {query: good_query(dog.id, skill.id)}
         response_body = JSON.parse(response.body, symbolize_names: true)
 
         # Test response shape 
+        # binding.pry
         expect(response_body.keys.count).to eq 1
         expect(response_body[:data].keys.count).to eq 1
         expect(response_body[:data][:passDogSkill].keys.count).to eq 2
@@ -36,31 +37,37 @@ module Mutations
         dog = create(:dog, user_id: user.id)
         skill = Skill.create!(name: "Oh You", level: 3 , description: "Running and Barking will no longer annoy your owners", criteria: " When running and barking, humans should not correct the behavior, but should sign and say 'oh, you....'" , youtube_link: "AtuxAJjygO8", created_at: Time.parse("2020.07.12"), updated_at: Time.parse("2020.07.12"))
         dog_skill = dog.dog_skills.create!(passed: false, dog_id: dog.id, skill_id: skill.id, created_at: Time.parse("2022.07.12"), updated_at: Time.parse("2022.07.12"))
-        
-        post "/graphql", params: {query: bad_query}
+        # Errors with bad dog_id
+        post "/graphql", params: {query: good_query(dog.id+9999, skill.id)}
         response_body = JSON.parse(response.body, symbolize_names: true)
         expect(response_body[:data][:passDogSkill]).to be nil
-        expect(response_body[:errors].first[:message]).to eq "Bad ID: dog_skill not found!"
+        expect(response_body[:errors].first[:message]).to eq "DogSkill Not Found: Your given dog_id and skill_id do not have a matching dog_skill. Please check your inputs"
+        # Same error with bad skill_id
+        post "/graphql", params: {query: good_query(dog.id+9999, skill.id)}
+        response_body = JSON.parse(response.body, symbolize_names: true)
+        expect(response_body[:data][:passDogSkill]).to be nil
+        expect(response_body[:errors].first[:message]).to eq "DogSkill Not Found: Your given dog_id and skill_id do not have a matching dog_skill. Please check your inputs"
       end
 
-      it "update a dog skill's 'passed' status to true" do
+      it "warns if a dog_skill's 'passed' was already true" do
         user = create(:user)
         dog = create(:dog, user_id: user.id)
         skill = Skill.create!(name: "Oh You", level: 3 , description: "Running and Barking will no longer annoy your owners", criteria: " When running and barking, humans should not correct the behavior, but should sign and say 'oh, you....'" , youtube_link: "AtuxAJjygO8", created_at: Time.parse("2020.07.12"), updated_at: Time.parse("2020.07.12"))
         dog_skill = dog.dog_skills.create!(passed: false, dog_id: dog.id, skill_id: skill.id, created_at: Time.parse("2022.07.12"), updated_at: Time.parse("2022.07.12"))
         
-        post "/graphql", params: {query: good_query(dog_skill.id)}
-        post "/graphql", params: {query: good_query(dog_skill.id)}
+        post "/graphql", params: {query: good_query(dog.id, skill.id)}
+        post "/graphql", params: {query: good_query(dog.id, skill.id)}
         response_body = JSON.parse(response.body, symbolize_names: true)
         
         expect(response_body[:data][:passDogSkill][:errors]).to eq ["Warning: 'pass' was already set to 'true' for this dog_skill"]        
       end
 
-      def good_query(id)
+      def good_query(dog_id_input, skill_id_input)
         <<~GQL
         mutation {
           passDogSkill(input: {
-            dogSkillId: #{id}
+            dogIdInput: #{dog_id_input}
+            skillIdInput: #{skill_id_input}
           }) {
               dogSkill {
                 id
